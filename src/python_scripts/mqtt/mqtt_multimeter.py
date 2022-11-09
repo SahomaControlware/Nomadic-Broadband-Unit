970d3ae404ec17f8c08beb5269f1b4b65d60e200
joelt77 <joeltanig77@gmail.com>
2022-11-08 17:01:34 -0800

    added .gitignore

--- src/python_scripts/mqtt/mqtt_multimeter.py
+++ src/python_scripts/mqtt/mqtt_multimeter.py
@@ -2,23 +2,42 @@ import paho.mqtt.client as mqtt
 import json
 import random as rand
 
-TOPIC_NAME_RECIEVE = "multimeter_request"
-TOPIC_NAME_SEND = "multimeter_reading"
+TOPIC_NAME_RECIEVE = "multimeter_request" #listen out for this and respond back with mm reading data
+TOPIC_NAME_SEND = "multimeter_status"
+HEARTBEAT_TOPIC_SEND = "heartbeat_status" 
+HEARTBEAT_TOPIC_RECIEVE = "heartbeat_request" #listen for this and respond back with status
 
 def multimeter_read(client, user_data, msg):
+    """_summary_
+    Checks for heartbeat 
+    Args:
+        client (_type_): _description_
+        user_data (_type_): _description_
+        msg (_type_): _description_
+    """
+    key = {
+        "heartbeat_multimeter":"1"
+    }
+
+    if msg.topic == 'heartbeat_request':
+        client.publish(HEARTBEAT_TOPIC_SEND, key)
+
+    if msg.topic == 'multimeter_request':
+        voltage = str(round(12 + rand.random(),2))
+        print(voltage)
+        result = client.publish(TOPIC_NAME_SEND, voltage)
+
     #Payload gets the actual content of the m/ui/essage, decoding it converts it from a byte array to JSON object
     decoded_msg = msg.payload.decode() 
     #Turns the JSON string into a python dictionary for easy map parsing
     decoded_msg = json.loads(decoded_msg) 
-    print("We got a message")
+    # print("We got a message")
     print(decoded_msg)
-    for pins in decoded_msg:
-        print(pins)
-        print(decoded_msg[pins])
-    voltage = str(round(12 + rand.random(),2))
-    result = client.publish(TOPIC_NAME_SEND, voltage)
+    # for pins in decoded_msg:
+    #     print(pins)
+        # print(decoded_msg[pins])
 
-    print(result.rc)    
+    # print(result.rc)    
     # if result != 0:
     #     print("Topic has sent")
     # else:
@@ -30,7 +49,7 @@ def on_connect_response(client, user_data, flags, rc):
     client.subscribe(TOPIC_NAME_RECIEVE)
 
 def on_publish(client, userdata, mid):
-    print(userdata)
+    # print(userdata)
     pass
 
 #publish a message when the topic is received
